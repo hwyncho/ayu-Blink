a50fe0aec717cf7fe4b92e838e68efba4900c27d
hwyncho <hwyn.cho@outlook.com>
2019-09-05 20:39:47 +0900

    :heavy_plus_sign: Add files

---
+++ ayu-Light.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#f07171", "#86b300", "#f2ae49", "#399ee6", "#a37acc", "#4cbf99", "#c7c7c7", "#686868", "#f07171", "#86b300", "#f2ae49", "#399ee6", "#a37acc", "#4cbf99", "#d1d1d1"]);
+t.prefs_.set('foreground-color', "#6c7680");
+t.prefs_.set('background-color', "#fafafa");
+t.prefs_.set('cursor-color', 'rgba(255,153,64,1.0)');
---
+++ ayu-Mirage.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#1a1e29", "#f28779", "#bae67e", "#ffd580", "#73d0ff", "#d4bfff", "#95e6cb", "#c7c7c7", "#686868", "#f28779", "#bae67e", "#ffd580", "#73d0ff", "#d4bfff", "#95e6cb", "#ffffff"]);
+t.prefs_.set('foreground-color', "#cbccc6");
+t.prefs_.set('background-color', "#1f2430");
+t.prefs_.set('cursor-color', 'rgba(255,204,102,1.0)');
